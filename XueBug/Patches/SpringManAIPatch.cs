3a59bbea75e88eaa1a5e854fb97fbbb5695582ce
Rares Macovei <rares_mac1@yahoo.com>
2024-03-27 22:57:36 +0200

    v0.1.5

---
+++ XueBug/Patches/SpringManAIPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using XueBug.Plugins;
+
+namespace XueBug.Patches
+{
+    [HarmonyPatch(typeof(SpringManAI))]
+    internal class SpringManAIPatch
+    {
+        [HarmonyPatch("__initializeVariables")]
+        [HarmonyPostfix]
+        public static void SpringManAudioPatch(ref AudioClip[] ___springNoises)
+        {
+            ___springNoises = XueBugBase.new_springNoises;
+        }
+    }
+}
